260d21e9a4981ec7b66de49b55ecec32bf2dd717
Jiss John <jiss.johnm@yahoo.com>
2015-03-25 03:51:30 -0700

    Display coupons data

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,16 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   request('http://api.bluepromocode.com/v2/promotions', function(error, response, body) {
+  	var couponsCount = 0, iter;
   	if(!error && response && response.statusCode === 200) {
   		body = JSON.parse(body);
+  		couponsCount = body.promotions && body.promotions.length;
+  		for(iter = 0; iter < couponsCount; iter++) {
+  			if(body.promotions[iter].promoType === 'link')
+  				body.promotions[iter].baseLink = 'https://bluepromocode.com/sale/'
+  			else
+  				body.promotions[iter].baseLink = 'https://bluepromocode.com/coupon/'
+  		}
   		res.render('index', {coupons: body.promotions});
   	} else {
   		console.log(error);
